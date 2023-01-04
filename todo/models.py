e02b9f9f54d1b012b3bdd86437a88d8731f452d9
Cihat <copurcihat@gmail.com>
2023-01-04 17:09:18 +0300

    6

--- todo/models.py
+++ todo/models.py
@@ -8,7 +8,7 @@ class Todo(models.Model):
         (3 ,'Low')
     )
     task=models.CharField(max_length=50)
-    description = models.CharField(blank=True)
+    description = models.CharField(max_length=50, blank=True)
     priority = models.SmallIntegerField(choices=PRIORITY , default=3)
     is_done= models.BooleanField(default=False)
     updated_date= models.DateTimeField(auto_now=True)
