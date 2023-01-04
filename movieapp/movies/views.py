3b25b89475c82dadbcc74c27d83800ccd8efc171
Cihat <copurcihat@gmail.com>
2023-01-04 08:52:06 +0300

    2

--- movieapp/movies/views.py
+++ movieapp/movies/views.py
@@ -9,18 +9,18 @@ film_liste= [
     {
         'film_adi':'film 1',
         'aciklama':'film 1 aciklama',
-        'resim'  : 'https://picsum.photos/200/300?random=1'
+        'resim'  : "https://picsum.photos/id/684/600/400"
         },
           {
         'film_adi':'film 2',
         'aciklama':'film 2 aciklama',
-        'resim'  : 'https://picsum.photos/200/300?random=2'
+        'resim'  : "https://picsum.photos/id/684/600/400"
         },
           {
         'film_adi':'film 3',
         'aciklama':'film 3 aciklama',
-        'resim'  : 'https://picsum.photos/200/300?random=4'
-        },
+        'resim'  : "https://picsum.photos/id/684/600/400"
+        }
 ]
 
 
@@ -34,4 +34,6 @@ def home(request):
 
 
 def movies(request):
-    return render(request , 'movies.html')
+    data= {
+        'filmler ' : film_liste }
+    return render(request , 'movies.html' , data)
